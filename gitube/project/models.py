f7161a33292f78b3eab6d84ae2e276464c02016d
harryxu <harryzhxu@gmail.com>
2010-01-21 01:11:10 +0800

    Continue defining models.

--- gitube/project/models.py
+++ gitube/project/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Permission, Group
 
 class Project(models.Model):
     name        = models.CharField(max_length=50, unique=True)
@@ -7,7 +7,7 @@ class Project(models.Model):
     user_id     = models.ForeignKey(User)
     create_at   = models.DateTimeField(auto_now_add=True)
     update_at   = models.DateTimeField(auto_now=True)
-    slug        = models.CharField(max_length=255, unique=True)
+    slug        = models.SlugField(max_length=255, unique=True)
 
 class Repository(models.Model):
     name        = models.CharField(max_length=50, unique=True)
@@ -17,7 +17,15 @@ class Repository(models.Model):
     create_at   = models.DateTimeField(auto_now_add=True)
     update_at   = models.DateTimeField(auto_now=True)
     is_public   = models.BooleanField(default=0)
+
+class Team(models.Model):
+    name        = models.CharField(max_length=50, unique=True)
+    description = models.TextField()
+    user_id     = models.ForeignKey(User)
+    users       = models.ManyToManyField(User)
         
-class RepositoryUserPermissions(models.Model):
-    pass
+class RepositoryUserRoles(models.Model):
+    repo_id  = models.ForeignKey(Repository)
+    user_id  = models.ForeignKey(User)
+    group_id = models.ForeignKey(Group)
 
