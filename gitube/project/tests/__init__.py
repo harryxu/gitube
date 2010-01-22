863a6415c8fcefab1bbc705603ecc99ee83df6f5
harryxu <harryzhxu@gmail.com>
2010-01-23 01:30:14 +0800

    add tests

---
+++ gitube/project/tests/__init__.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+from gitube.project.tests import *
+
+class RepositoryTest(TestCase):
+    fixtures = ['testing_users.yaml']
+
+    def setUp(self):
+        pass
+
+    def testCanRead(self):
+        pass
+    
