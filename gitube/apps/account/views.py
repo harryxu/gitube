7032f726da4f631bc9371324dbaf423ba1048243
harryxu <harryzhxu@gmail.com>
2010-01-27 12:55:43 +0800

    Set new user inactive.

--- gitube/apps/account/views.py
+++ gitube/apps/account/views.py
@@ -1,17 +1,16 @@
-from django.contrib.auth.models import User
-
 import django_authopenid
 from django_authopenid.views import not_authenticated
 
 
 @not_authenticated
 def register(request):
-    django_authopenid.views.register_account = register_account
-    return django_authopenid.views.register(request, send_email=False)
+    result = django_authopenid.views.register(request, send_email=False)
 
-def register_account(form, _openid):
-    user = User.objects.create_user(form.cleaned_data['username'], 
-                            form.cleaned_data['email'])
-    user.is_active = False
-    user.save()
+    if (request.POST and request.user is not None):
+        # We need active user by admin.
+        request.user.is_active = False
+        request.user.save()
+
+    return result
+
     
