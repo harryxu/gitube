f9838a55a5740e44789a5ba1647ad36085fe68d9
harryxu <harryzhxu@gmail.com>
2010-02-28 16:42:45 +0800

    url for home page.

--- gitube/apps/project/views.py
+++ gitube/apps/project/views.py
@@ -1,1 +1,5 @@
-# Create your views here.
+from django.shortcuts import get_object_or_404, render_to_response
+
+def home(request):
+    """docstring for start"""
+    return render_to_response('home.html')
--- gitube/urls.py
+++ gitube/urls.py
@@ -2,16 +2,18 @@ from django.conf import settings
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
 
-from gitube.apps.account import views as accountViews
-from gitube.apps import project
-
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^account/register/$', accountViews.register, name='user_register'),
+    # home page
+    url(r'^$', 'apps.project.views.home', name='home'),
+
+    # register page
+    url(r'^account/register/$', 'apps.account.views.register', name='user_register'),
+
     (r'^account/', include('django_authopenid.urls')),
     (r'^admin/', include(admin.site.urls)),
-    (r'^p/', include(project.urls)),
+    (r'^p/', include('apps.project.urls')),
 )
 
 if settings.DEBUG:
