b2b246b4dd1221c50ad9ce01b1fe9091dec695fb
harryxu <harryzhxu@gmail.com>
2010-03-15 01:03:47 +0800

    Prepare test file for ssh.

---
+++ gitube/tools/test.py
@@ -0,0 +1,23 @@
+import unittest
+import ssh
+
+class TestSSH(unittest.TestCase):
+    def setUp(self):
+        """Create mockup file"""
+        pass
+
+    def tearDown(self):
+        """Delete mockup file"""
+        pass
+
+    def test_write_key(self):
+        self.fail('not implement')
+
+    def test_remove_key(self):
+        self.fail('not implement')
+
+def main():
+    unittest.main()
+
+if __name__ == '__main__':
+    main()
