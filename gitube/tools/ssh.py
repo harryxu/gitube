52c69c8a7b24dc4320af5dc831e13fc5f52f9346
harryxu <harryzhxu@gmail.com>
2010-03-16 00:50:45 +0800

    SSH key make and write.

--- gitube/tools/ssh.py
+++ gitube/tools/ssh.py
@@ -1,3 +1,15 @@
+#TODO lock file before write or remove key.
 
+def makeAuthorizedKey(user, key):
+    template = ('command="gitube-serve %(user)s",no-port-forwarding,'
+              +'no-X11-forwarding,no-agent-forwarding,no-pty %(key)s')
+    return template % {'user':user, 'key':key}
 
+def writeKey(path, user, key):
+    """docstring for writeKey"""
+    fd = open(path, 'a')
+    fd.write(makeAuthorizedKey(user, key)+'\n')
+    fd.close()
 
+def removeKey(path, user, key):
+    pass
--- gitube/tools/test.py
+++ gitube/tools/test.py
@@ -1,17 +1,20 @@
 import unittest
 import ssh
 
-class TestSSH(unittest.TestCase):
-    def setUp(self):
-        """Create mockup file"""
-        pass
+key1 = "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAnNt3hQjf1KApe7qPhg7lE2+HOS8VO/nqteTd83BGY86+2lQhXyVN4bzwN0baRc+A/OZMKbnZ9amIdaoOsusOJGEpcx0hIr4XsVAvO/iOu+byPTtoxSxDP4GBj9N50BN+mMgjsf63jjrynL9Q3gIk+t9MY7lxB0+hVQlQvE0LRMtRw0vp21Fe4nGxN0zg4YlKBVqSYpORrGbh3T/UBHRxYikTEm2mPbVzwBAo+3Tl/129Vlv/6eRFcfrmHPc1wK1+l95H/ct+hCRkyhBjRLWVQ24RTjebvS1ndUBh5e4YoN7S7POPhyvKdCx2I9/hx9CSyF1EW8Hsghpl3Pka8TOovw== harry@harry-laptop"
 
+class TestSSH(unittest.TestCase):
     def tearDown(self):
         """Delete mockup file"""
         pass
 
+    def test_makeAuthorizedKey(self):
+        """docstring"""
+        theKey = 'command="gitube-serve harry",no-port-forwarding,no-X11-forwarding,no-agent-forwarding,no-pty ' + key1
+        self.assertEqual(theKey, ssh.makeAuthorizedKey('harry', key1))
+
     def test_write_key(self):
-        self.fail('not implement')
+        ssh.writeKey('/tmp/asss', 'harry', key1)
 
     def test_remove_key(self):
         self.fail('not implement')
