463702636f8b3bf82d7c804562ee2511b5fa92cb
harryxu <harryzhxu@gmail.com>
2010-03-16 23:32:45 +0800

    Move some code to setUp and tearDown in test.

--- gitube/tools/test.py
+++ gitube/tools/test.py
@@ -6,39 +6,42 @@ key1 = "ssh-rsa AAAA"
 key2 = "ssh-rsa BBBB"
 key3 = "ssh-rsa CCCC"
 
+k1 = ssh.makeAuthorizedKey('harry', key1) + '\n'
+k2 = ssh.makeAuthorizedKey('harry', key2) + '\n'
+k3 = ssh.makeAuthorizedKey('harry', key3) + '\n'
+
 class TestSSH(unittest.TestCase):
+    def setUp(self):
+        self.tmp = self.getTmp()
+
     def tearDown(self):
         """Delete mockup file"""
-        pass
+        if os.path.exists(self.tmp):
+            os.remove(self.tmp)
 
     def test_makeAuthorizedKey(self):
         """docstring"""
-        theKey = 'command="gitube-serve harry",no-port-forwarding,no-X11-forwarding,no-agent-forwarding,no-pty ' + key1
+        theKey = 'command="gitube-serve harry",no-port-forwarding,no-X11-forwarding,no-agent-forwarding,no-pty %s' % key1
         self.assertEqual(theKey, ssh.makeAuthorizedKey('harry', key1))
 
     def test_write_key(self):
-        tmp = self.getTmp()
+        tmp = self.tmp
         ssh.writeKey(tmp, 'harry', key1)
         ssh.writeKey(tmp, 'harry', key2)
         ssh.writeKey(tmp, 'harry', key3)
 
         f = open(tmp, 'r')
-        self.assertEqual(ssh.makeAuthorizedKey('harry', key1)+'\n', f.readline())
-        self.assertEqual(ssh.makeAuthorizedKey('harry', key2)+'\n', f.readline())
-        self.assertEqual(ssh.makeAuthorizedKey('harry', key3)+'\n', f.readline())
+        self.assertEqual(k1, f.readline())
+        self.assertEqual(k2, f.readline())
+        self.assertEqual(k3, f.readline())
 
         f.close()
-        os.remove(tmp)
 
     def test_remove_key(self):
-        tmp = self.getTmp()
+        tmp = self.tmp
         ssh.writeKey(tmp, 'harry', key1)
         ssh.writeKey(tmp, 'harry', key2)
         ssh.writeKey(tmp, 'harry', key3)
-        
-        k1 = ssh.makeAuthorizedKey('harry', key1) + '\n'
-        k2 = ssh.makeAuthorizedKey('harry', key2) + '\n'
-        k3 = ssh.makeAuthorizedKey('harry', key3) + '\n'
 
         f = open(tmp, 'r')
         self.assertEqual(f.readlines(), [k1, k2, k3])
@@ -48,8 +51,6 @@ class TestSSH(unittest.TestCase):
         f = open(tmp, 'r')
         self.assertEqual(f.readlines(), [k1, k3])
         f.close()
-
-        os.remove(tmp)
 
     def getTmp(self):
         return '/tmp/gitube_test_%d.tmp' % os.getpid()
