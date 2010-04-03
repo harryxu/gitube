9314e0b616a82e35233b4c2c3f90b717fbe3b4f5
harryxu <harryzhxu@gmail.com>
2010-04-03 13:39:53 +0800

    access

--- gitube/tools/access.py
+++ gitube/tools/access.py
@@ -1,8 +1,20 @@
-#from gitube.apps.project.models import Project, Repository
-#from django.contrib.auth.models import User
+import hashlib
+
+from gitube.apps.project.models import Project, Repository
+from django.contrib.auth.models import User
 
 def haveAccess(config, user, mode, path):
     """ Access controll """
-    #theUser = User.objects.get(username=user)
+    myuser = User.objects.get(username=user)
+    if not myuser:
+        return None
+
+    pathHash = hashlib.sha1(path).hexdigest()
+    repo = Repository.objects.get(path_hash=pathHash)
+
+    if not repo:
+        return None
+
+    
 
     
