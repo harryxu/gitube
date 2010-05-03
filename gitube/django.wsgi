29e163548d129338dbf19fb11bad78a9591255be
harryxu <harryzhxu@gmail.com>
2010-05-03 16:44:41 +0800

    style adjust and git address copier.

--- gitube/django.wsgi
+++ gitube/django.wsgi
@@ -2,6 +2,13 @@
 
 import os
 import sys
+import site
+
+root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+ 
+site.addsitedir(os.path.join(root_dir, '/home/harry/.virtualenvs/dev/lib/python2.6/site-packages'))
+ 
+sys.path.append(root_dir)
 
 os.environ['PYTHON_EGG_CACHE'] = '/tmp/.python-eggs'
 os.environ['DJANGO_SETTINGS_MODULE'] = 'gitube.settings'
---
+++ gitube/static/js/copier.js
@@ -0,0 +1,12 @@
+$(function(){
+    var params = { wmode: "transparent" };
+    var attributes = {};
+    $('.git-addr').each(function(i){
+        var self = $(this);
+        self.after('<span id="copier'+ i +'"></span>')
+        var flashvars = {};
+        flashvars.s = self.text();
+        swfobject.embedSWF("/static/copier.swf", "copier"+i, "16", "16", "9.0.0", 
+                            false, flashvars, params, attributes);
+    });
+});
