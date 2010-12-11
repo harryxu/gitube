66568e0dae175e0c17354bf7cfe6cd565775efb9
harryxu <harryzhxu@gmail.com>
2010-12-11 11:20:59 +0800

    Add fancybox js to popup gitweb.

--- gitube/static/js/gitube.js
+++ gitube/static/js/gitube.js
@@ -1,4 +1,5 @@
 $(function(){
+    // embed swf for copy git url
     var params = { wmode: "transparent" };
     var attributes = {};
     $('.git-addr').each(function(i){
@@ -8,5 +9,14 @@ $(function(){
         flashvars.s = self.text();
         swfobject.embedSWF("/static/copier.swf", "copier"+i, "16", "16", "9.0.0", 
                             false, flashvars, params, attributes);
+    });
+
+    // gitweb iframe
+    $("a.iframe").fancybox({   
+        'width'             : '96%',
+        'height'            : '96%',
+        'autoScale'         : true,
+        'type'              : 'iframe',
+        'hideOnContentClick': false
     });
 });
