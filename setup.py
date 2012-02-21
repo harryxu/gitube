90a471ee76d8d056e4db9f005179cddd980821b5
harryxu <harryzhxu@gmail.com>
2012-02-21 23:46:30 +0800

    Add setup files.

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(name='gitube',
+      version=version,
+      description="Web interface for git repo manager.",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='git',
+      author='harryxu',
+      author_email='harryzhxu@gmail.com',
+      url='https://github.com/harryxu/gitube',
+      license='bsd',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'django>=1.3',
+          'django-authopenid',
+          'django-registration',
+          'gitosis',
+      ],
+      dependency_links = [
+          'http://eagain.net/gitweb/?p=gitosis.git;a=snapshot;h=dedb3dc63f413ed6eeba8082b7e93ad136b16d0d;sf=tgz#egg=gitosis',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
