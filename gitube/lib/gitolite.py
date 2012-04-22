e85d62b7d70513d7d9eb6b256c6fdd8c70af8f11
harryxu <harryzhxu@gmail.com>
2012-04-23 00:45:51 +0800

    basic gitolite Conf class implement.

--- gitube/lib/gitolite.py
+++ gitube/lib/gitolite.py
@@ -9,7 +9,44 @@ class Conf(object):
     """gitolite conf file management """
 
     def __init__(self, filename='gitolite.conf'):
-        self._filename = filename
+        self.filename = filename
+        self.groups = {}
+        self.repos = {}
+
+    def add_group(self, group):
+        self.groups[group.name] = group
+
+    def del_group(self, group_name):
+        del self.groups[group_name]
+
+    def has_group(self, group_name):
+        return group_name in self.groups
+
+    def get_group(self, group_name):
+        if not self.has_groupd(group_name):
+            return None
+        return self.groups[group_name]
+
+    def add_repo(self, repo):
+        self.repos[repo.name] = repo
+
+    def del_repo(self, repo_name):
+        del self.repos[repo_name]
+
+    def has_repo(self, repo_name):
+        return repo_name in self.repos
+
+    def get_repo(self, repo_name):
+        if not self.has_repo(repo_name):
+            return None
+        return self.repos[repo_name]
+
+    def __str__(self):
+        groups = [str(group) for group in self.groups.values()]
+        repos = [str(repo) for repo in self.repos.values()]
+
+        return '%s\n\n%s' % ('\n'.join(groups), '\n\n'.join(repos))
+
 
 class Group(object):
     def __init__(self, name):
@@ -50,5 +87,5 @@ class Repo(object):
                 del self.permissions[user + '-']
 
     def __str__(self):
-        users = [v for k, v in self.permissions.items()]
+        users = [perm for perm in self.permissions.values()]
         return 'repo %s\n    %s' % (self.name, '\n    '.join(users))
--- gitube/test/gitolite_test.py
+++ gitube/test/gitolite_test.py
@@ -4,6 +4,39 @@
 import unittest
 from gitube.lib import gitolite
 
+class ConfTest(unittest.TestCase):
+
+    def test_misc(self):
+        group_admin = gitolite.Group('admin')
+        group_admin.add_user('boss')
+
+        group_dev = gitolite.Group('developer')
+        group_dev.add_user('harry')
+        group_dev.add_user('harryxu')
+
+        repo_gitube = gitolite.Repo('gitube')
+        repo_gitube.add_permission('RW', '', 'harry', 'harryxu')
+        repo_gitube.add_permission('-', 'master', 'harry')
+
+        repo_test = gitolite.Repo('test')
+        repo_test.add_permission('RW', '', 'harry', 'harryxu')
+
+        conf = gitolite.Conf();
+        conf.add_group(group_admin)
+        conf.add_group(group_dev)
+        conf.add_repo(repo_gitube)
+        conf.add_repo(repo_test)
+
+        self.assertTrue(conf.has_group('developer'))
+        self.assertTrue(conf.has_group('admin'))
+        conf.del_group('developer')
+        self.assertFalse(conf.has_group('developer'))
+
+        self.assertTrue(conf.has_repo('gitube'))
+        self.assertTrue(conf.has_repo('test'))
+        conf.del_repo('test')
+        self.assertFalse(conf.has_group('test'))
+
 class GroupTest(unittest.TestCase):
 
     def test_add_user(self):
@@ -23,7 +56,6 @@ class GroupTest(unittest.TestCase):
         group.del_user('harry')
 
         self.assertEqual(str(group), 'developer = flash')
-        
 
 class RepoTest(unittest.TestCase):
 
