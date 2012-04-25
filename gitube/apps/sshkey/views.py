42fbf933eeac0ff7b46917ffed84a2108a6c913e
harryxu <harryzhxu@gmail.com>
2012-04-26 00:32:20 +0800

    Updated README.

--- gitube/apps/sshkey/views.py
+++ gitube/apps/sshkey/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import get_object_or_404, render_to_response, redirect
-from django.http import Http404
 from django.contrib.auth.decorators import login_required
 from django.template import RequestContext
 
