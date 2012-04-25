383152f8b73186a6f8e2553d587936f0c653f414
harryxu <harryzhxu@gmail.com>
2012-04-26 00:53:18 +0800

    Clean some code.

--- gitube/apps/account/views.py
+++ gitube/apps/account/views.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.decorators import login_required
-from django.http import Http404
-from django.shortcuts import get_object_or_404, render_to_response, redirect
+from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 import django_authopenid
